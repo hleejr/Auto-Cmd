225279df45193d4c1868076827f115d2b0dc0a55
Henry <hleejr0103@gmail.com>
2020-07-06 13:39:44 -0700

    added page translation

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module makesite
 
 go 1.13
+
+require (
+	cloud.google.com/go v0.60.0 // indirect
+	github.com/bregydoc/gtranslate v0.0.0-20191225024717-45b28d4fda86
+	github.com/robertkrimen/otto v0.0.0-20191219234010-c382bd3c16ff // indirect
+	go.opencensus.io v0.22.4 // indirect
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200706141556-5779274c8e96 // indirect
+	google.golang.org/grpc v1.30.0 // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
--- makesite.go
+++ makesite.go
@@ -1,5 +1,74 @@
 package main
 
+// GO111MODULE=""
+// GOPATH="/Users/hennybowejr/go"
+// GOROOT="/usr/local/go"
+
+// Afrikaans	af
+// Irish	ga
+// Albanian	sq
+// Italian	it
+// Arabic	ar
+// Japanese	ja
+// Azerbaijani	az
+// Kannada	kn
+// Basque	eu
+// Korean	ko
+// Bengali	bn
+// Latin	la
+// Belarusian	be
+// Latvian	lv
+// Bulgarian	bg
+// Lithuanian	lt
+// Catalan	ca
+// Macedonian	mk
+// Chinese Simplified	zh-CN
+// Malay	ms
+// Chinese Traditional	zh-TW
+// Maltese	mt
+// Croatian	hr
+// Norwegian	no
+// Czech	cs
+// Persian	fa
+// Danish	da
+// Polish	pl
+// Dutch	nl
+// Portuguese	pt
+// English	en
+// Romanian	ro
+// Esperanto	eo
+// Russian	ru
+// Estonian	et
+// Serbian	sr
+// Filipino	tl
+// Slovak	sk
+// Finnish	fi
+// Slovenian	sl
+// French	fr
+// Spanish	es
+// Galician	gl
+// Swahili	sw
+// Georgian	ka
+// Swedish	sv
+// German	de
+// Tamil	ta
+// Greek	el
+// Telugu	te
+// Gujarati	gu
+// Thai	th
+// Haitian Creole	ht
+// Turkish	tr
+// Hebrew	iw
+// Ukrainian	uk
+// Hindi	hi
+// Urdu	ur
+// Hungarian	hu
+// Vietnamese	vi
+// Icelandic	is
+// Welsh	cy
+// Indonesian	id
+// Yiddish	yi
+
 import (
 	"flag"
 	"fmt"
@@ -7,6 +76,8 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/bregydoc/gtranslate"
 )
 
 type content struct {
@@ -22,8 +93,28 @@ func readFile(name string) string {
 	return string(fileContents)
 }
 
-func createHTML(filename string) {
+func translate(content string, input string, desired string) string {
+	translated, err := gtranslate.TranslateWithParams(
+		content,
+		gtranslate.TranslationParams{
+			From: input,
+			To:   desired,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Printf("%s: %s | %s: %s \n", input, content, desired, translated)
+	// en: Hello World | ja: こんにちは世界
+	return translated
+}
+
+func createHTML(filename string, input string, desired string) {
 	data := readFile(filename)
+	if input != "" {
+		data = translate(data, input, desired)
+	}
 	content := content{Text: data}
 	temp := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 
@@ -55,11 +146,13 @@ func createFileFromTemp(filename string) {
 
 	inputFile := flag.String("file", "", "what file are you trying to convert?")
 	inputDir := flag.String("dir", "", "In what directory are your text files?")
+	inputLang := flag.String("lang", "", "Do you need to translate these files? What language are they in now? See language abbreviations for appropriate entries")
+	desired := flag.String("trans", "", "What language do you want ot translate to? See language abbreviations for appropriate entries. EX) ja for Japanese")
 	flag.Parse()
 
 	if *inputFile != "" {
 
-		createHTML(*inputFile)
+		createHTML(*inputFile, *inputLang, *desired)
 
 	} else if *inputDir != "" {
 
@@ -71,7 +164,7 @@ func createFileFromTemp(filename string) {
 
 		for _, file := range files {
 			if strings.Contains(file.Name(), ".txt") {
-				createHTML(file.Name())
+				createHTML(file.Name(), *inputLang, *desired)
 			}
 		}
 	} else {
