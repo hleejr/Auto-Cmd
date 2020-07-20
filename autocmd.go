c0dae799cf9e87bc3fb69de65a16694a11c13b43
Henry <hleejr0103@gmail.com>
2020-07-19 22:36:33 -0700

    testing cmds after go installation

--- autocmd.go
+++ autocmd.go
@@ -39,10 +39,7 @@ func readFile(name string) string {
 	return string(fileContents)
 }
 
-func runCommands(filename string) {
-	inputMsg := flag.String("msg", "commit made", "do you have a message for your commit?")
-	inputBranch := flag.String("branch", "master", "what branch are you committing to?")
-	flag.Parse()
+func runCommands(filename string, message string, branch string) {
 
 	result := readFile(filename)
 	cmds := strings.Split(result, "\n")
@@ -59,10 +56,10 @@ func runCommands(filename string) {
 		}
 
 		if strings.Contains(cmd, "commit") {
-			command.Args = append(command.Args, *inputMsg)
+			command.Args = append(command.Args, message)
 		}
 		if strings.Contains(cmd, "push") {
-			command.Args = append(command.Args, *inputBranch)
+			command.Args = append(command.Args, branch)
 		}
 
 		err := command.Run()
@@ -76,6 +73,10 @@ func runCommands(filename string) {
 }
 
 func main() {
-	inputFile := flag.String("file", "git.txt", "what file has the commands you want to run?")
-	runCommands(*inputFile)
+	inputFile := flag.String("file", "", "what file has the commands you want to run?")
+	inputMsg := flag.String("msg", "commit made", "do you have a message for your commit?")
+	inputBranch := flag.String("branch", "master", "what branch are you committing to?")
+	flag.Parse()
+
+	runCommands(*inputFile, *inputMsg, *inputBranch)
 }
